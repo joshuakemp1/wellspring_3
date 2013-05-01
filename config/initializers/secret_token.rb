62500a3f517532847a9e1ccb4f62a7143d74e862
joshua kemp <joshuakemp85@gmail.com>
2013-05-01 10:37:41 -0400

    adding app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wellspring3::Application.config.secret_token = '6571b3a1b21b4e8cf4315ad654d1b671630783409bb6b42083ec10d7258caf3745a9e970d4e59c8e65ee2e0dee88ef8b0aa316ee4b8a6d85ac1f21cd77d60a59'
